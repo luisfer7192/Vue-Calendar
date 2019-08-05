7c9fccb996b4a616701c619de6ab13196f23db8e
Luis Fernando Salazar Buitrago <luisfer7192@hotmail.com>
2019-08-05 18:43:03 -0500

    add text to the home

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -3,7 +3,10 @@
     <div class="column is-half is-offset-one-quarter">
       <h4 class="centerText title">Welcome to my Calendar</h4>
       <p class="centerText">This is an example of a Calendar in VueJs.</p>
-      <p class="centerText">In the header go to Calendar.</p>
+      <p class="centerText">Using Vuex, Buefy, Puppeteer</p>
+      <p class="centerText">You can move between months</p>
+      <p class="centerText">Create, Edit, Delete events</p>
+      <p class="centerText">In the header you can go to Calendar.</p>
       <p class="centerText">I hope you enjoy it!.</p>
     </div>
   </div>
