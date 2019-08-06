d00b8f6ceb8f90804ea00999d2dcf35734ef79b6
Luis Fernando Salazar Buitrago <luisfer7192@hotmail.com>
2019-08-05 19:04:27 -0500

    Install jest

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+import Home from '@/components//Home'
+
+describe('Home', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'Welcome to my Calendar This is an example of a Calendar in VueJs. Using Vuex, Buefy, Puppeteer You can move between months Create, Edit, Delete events In the header you can go to Calendar. I hope you enjoy it!.'
+    const wrapper = shallowMount(Home)
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
